44da9ce318274e92d422ace9e884681903869de3
jsr <jaskomal2@gmail.com>
2020-11-06 21:45:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,17 @@
 # require modules here
-
+require 'yaml'
+require 'pry' 
 def load_library
   # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  emot_hash = load_library(file)
+  outpu = emot_hash["get_emoticon"][emoticon]
+  if outpu == nil
+    "Sorry, that emoticon was not found"
+  end
+  outpu
 end
 
 def get_english_meaning
